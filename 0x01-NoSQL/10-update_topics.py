aa8b2cd37bdd5081bd6298224e660108d866f995
Wangutusi Arshton <wangarshton@gmail.com>
2024-06-18 20:10:36 +0300

    Change school topics

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+update  school names by topic
+"""
+
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document"""
+    return mongo_collection.update_many({"name": name},
+                                        {"$set": {"topics": topics}})
